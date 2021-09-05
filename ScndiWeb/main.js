d1d9fd8e3c6ab1522d09bb52cd4bf26423f3b0b6
Artyom Damberg <zukill1@inbox.lv>
2021-09-05 20:54:40 +0300

    bugs fixed

--- ScndiWeb/main.js
+++ ScndiWeb/main.js
@@ -1,41 +1,41 @@
 $(document).ready(function () {
-  $('#n4').change(function () {
-    var val = $("#n4 option:selected").text();
+  $('#productType').change(function () {
+    var val = $("#productType option:selected").text();
     if (val == "") {
       $('.book').hide();
       $('.disc').hide();
       $('.furnit').hide();
-      $('#f1').val('');
-      $('#f2').val('');
-      $('#f3').val('');
-      $('#b1').val('');
-      $('#w1').val('');
+      $('#height').val('');
+      $('#width').val('');
+      $('#length').val('');
+      $('#size').val('');
+      $('#weight').val('');
     }
     if (val == "Furniture") {
       $('.book').hide();
       $('.disc').hide();
       $('.furnit').show();
-      $('#w1').val('');
-      $('#b1').val('');
+      $('#weight').val('');
+      $('#size').val('');
     }
-    if (val == "DVD-disc") {
+    if (val == "DVD") {
       $('.book').hide();
       $('.furnit').hide();
       $('.disc').show();
-      $('#f1').val('');
-      $('#f2').val('');
-      $('#f3').val('');
-      $('#b1').val('');
+      $('#height').val('');
+      $('#width').val('');
+      $('#length').val('');
+      $('#size').val('');
 
     }
     if (val == "Book") {
       $('.furnit').hide();
       $('.disc').hide();
       $('.book').show();
-      $('#f1').val('');
-      $('#f2').val('');
-      $('#f3').val('');
-      $('#w1').val('');
+      $('#height').val('');
+      $('#width').val('');
+      $('#length').val('');
+      $('#weight').val('');
     }
   });
 
--- ScndiWeb/register.php
+++ ScndiWeb/register.php
@@ -15,33 +15,33 @@
 <body>
   <h3>Product Add</h3>
   <div class="add">
-    <form id="#product_form" action="products.php" method="POST">
+    <form id="product_form" action="products.php" method="POST">
       <div class="button-wrapper">
         <button class="button submition" type="submit" name="submit">Save</button>
         <button id="button-cancel" class="button submition" name="cancel">Cancel</button>
       </div>
       <hr>
       <h4>SKU</h4>
-      <input id="n1" type="text" name="sku">
+      <input id="sku" type="text" name="sku">
       <br>
       <h4>Name</h4>
-      <input id="n2" type="text" name="name">
+      <input id="name" type="text" name="name">
       <br>
       <h4>Price</h4>
-      <input id="n3" type="text" name="price">
+      <input id="price" type="text" name="price">
       <br>
       <h4>Type Switcher</h4>
-      <select id="n4" name="choose">
+      <select id="productType" name="choose">
         <option value="" disabled selected>Type Switcher</option>
         <option value="furniture">Furniture</option>
-        <option value="book">Book</option>
-        <option value="dvd-disc">DVD-disc</option>
+        <option value="Book">Book</option>
+        <option value="DVD">DVD</option>
       </select><br>
 
       <form action="products.php" method="POST">
         <div class="disc">
           <h4>Size</h4>
-          <input id="b1" type="text" name="size"><br>
+          <input id="size" type="text" name="size"><br>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Phasellus semper lectus vitae lectus tempor dapibus.
             Donec suscipit vitae diam sed aliquam. Nunc mollis lorem at tellus rhoncus interdum vel
@@ -49,11 +49,11 @@
         </div>
         <div class="furnit">
           <h4>Height</h4>
-          <input id="f1" type="text" name="height"><br>
+          <input id="height" type="text" name="height"><br>
           <h4>Width</h4>
-          <input id="f2" type="text" name="width"><br>
+          <input id="width" type="text" name="width"><br>
           <h4>Length</h4>
-          <input id="f3" type="text" name="length"><br>
+          <input id="length" type="text" name="length"><br>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Phasellus semper lectus vitae lectus tempor dapibus.
             Donec suscipit vitae diam sed aliquam. Nunc mollis lorem at tellus rhoncus interdum vel
@@ -61,7 +61,7 @@
         </div>
         <div class="book">
           <h4>Weight</h4>
-          <input id="w1" type="text" name="weight"><br>
+          <input id="weight" type="text" name="weight"><br>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Phasellus semper lectus vitae lectus tempor dapibus.
             Donec suscipit vitae diam sed aliquam. Nunc mollis lorem at tellus rhoncus interdum vel
